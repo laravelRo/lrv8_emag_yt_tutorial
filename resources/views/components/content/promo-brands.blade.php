4db5ec9199a55cb42577f840e45ce27fdd1d9e1b
laravelRo <rarestudor@gmail.com>
2021-12-21 10:43:55 +0200

    Update promo-brands.blade.php

--- resources/views/components/content/promo-brands.blade.php
+++ resources/views/components/content/promo-brands.blade.php
@@ -2,15 +2,19 @@
     <div class="row px-xl-5">
         @foreach ($brands as $brand)
             <div class="col-md-6 pb-4">
-                <div class="position-relative bg-secondary text-center text-md-right text-white mb-2 py-5 px-5">
-                    <img src="{{ $brand->photoUrl() }}" alt="">
-                    <div class="position-relative" style="z-index: 1;">
-                        <h5 class="text-uppercase text-primary mb-3">{{ $brand->title }}</h5>
-                        <h1 class="mb-4 font-weight-semi-bold">{{ $brand->name }}</h1>
-                        <a href="{{ route('brand', $brand->slug) }}"
-                            class="btn btn-outline-primary py-md-2 px-md-3">Details</a>
+                <a href="{{ route('brand', $brand->slug) }}">
+                    <div class="position-relative bg-secondary text-center text-md-right text-white mb-2 py-5 px-5">
+
+                        <img src="{{ $brand->photoUrl() }}" alt="">
+
+                        <div class="position-relative" style="z-index: 1;">
+                            <h5 class="text-uppercase text-primary mb-3">{{ $brand->title }}</h5>
+                            <h1 class="mb-4 font-weight-semi-bold">{{ $brand->name }}</h1>
+                            <a href="{{ route('brand', $brand->slug) }}"
+                                class="btn btn-outline-primary py-md-2 px-md-3">Details</a>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
         @endforeach
     </div>

d1247c245b2c23f766b334c61f65ebf6577a2d5c
laravelRo <71215083+laravelRo@users.noreply.github.com>
2022-06-05 17:13:05 +0300

    Update order-items.blade.php

--- resources/views/livewire/admin/order-items.blade.php
+++ resources/views/livewire/admin/order-items.blade.php
@@ -4,31 +4,32 @@
                  cost:</b> {{ number_format($order->totalCost(), '2', ',', '.') }}
          </h4>
      </div>
-     <div class="row my-3">
-         <h5 class="text-info">Status</h5>
-         <div class="col-md-4 p-1 border rounded">
-             @if ($order->approved_at)
-                 <p class="text-primary">
-                     <b>Approved </b>&nbsp;&nbsp;
-                     <i class="fas fa-thumbs-up"></i> {{ $order->approved_at->format('d-M Y') }}
-                     <button wire:click.prevent="uncheckOrder" class="btn btn-sm btn-danger"><i
-                             class="fas fa-ban"></i></button>
-                 </p>
-             @else
-                 <p class="text-danger">
-                     <b>Pending approuval </b>&nbsp;&nbsp;
-                     <button wire:click.prevent="approveOrder" class="btn btn-sm btn-primary"><i
-                             class="fas fa-check-double"></i></button>
-                 </p>
-             @endif
-         </div>
-         <div class="col-md-4">
-
-         </div>
-         <div class="col-md-4"></div>
-     </div>
 
      <div class="card-body">
+         <div class="row my-3">
+             <h5 class="text-info">Status</h5>
+             <div class="col-md-4 p-1 border rounded">
+                 @if ($order->approved_at)
+                     <p class="text-primary">
+                         <b>Approved </b>&nbsp;&nbsp;
+                         <i class="fas fa-thumbs-up"></i> {{ $order->approved_at->format('d-M Y') }}
+                         <button wire:click.prevent="uncheckOrder" class="btn btn-sm btn-danger"><i
+                                 class="fas fa-ban"></i></button>
+                     </p>
+                 @else
+                     <p class="text-danger">
+                         <b>Pending approuval </b>&nbsp;&nbsp;
+                         <button wire:click.prevent="approveOrder" class="btn btn-sm btn-primary"><i
+                                 class="fas fa-check-double"></i></button>
+                     </p>
+                 @endif
+             </div>
+             <div class="col-md-4">
+
+             </div>
+             <div class="col-md-4"></div>
+         </div>
+
          <table class="table table-striped">
              <thead class="table-dark">
                  <tr>
